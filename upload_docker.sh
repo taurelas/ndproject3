6b86f0da985722921dc62ebaea16ff24f68060ed
maciej.beimcik <maciej.beimcik@intive.com>
2020-08-22 15:32:49 +0200

    Working uploading to DockerHub

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=mbeimcik/api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=mbeimcik 
+docker tag api $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
