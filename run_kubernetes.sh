87d6c23ec2ca5f2e8e9cce5d5a61cf0aa2525e30
maciej.beimcik <maciej.beimcik@intive.com>
2020-08-24 12:13:05 +0200

    Working k8s script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
-
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=mbeimcik/api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run api --generator=run-pod/v1 --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+sleep 5
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward api 80:80 
